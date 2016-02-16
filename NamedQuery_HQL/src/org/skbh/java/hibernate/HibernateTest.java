199dba4ac3fd364c712a0c36861e19bdf895241c
Sushil Kumar Bhaskar <write2sirfsushil@gmail.com>
2016-02-17 00:45:22 +0530

    code formatted

--- NamedQuery_HQL/src/org/skbh/java/hibernate/HibernateTest.java
+++ NamedQuery_HQL/src/org/skbh/java/hibernate/HibernateTest.java
@@ -14,12 +14,11 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 
-		SessionFactory sessionfactory = new Configuration().configure()
-				.buildSessionFactory();
+		SessionFactory sessionfactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionfactory.openSession();
 		session.beginTransaction();
-		Query query = session.createSQLQuery("SELECT * FROM userdetail")
-				.addEntity(UserDetail.class);
+		Query query = session.createSQLQuery("SELECT * FROM userdetail").addEntity(UserDetail.class);
+		@SuppressWarnings("unchecked")
 		List<UserDetail> empData = query.list();
 		for (UserDetail userDetail1 : empData) {
 			// emp.setUserId(Long.parseLong(row[0].toString()));
@@ -28,21 +27,23 @@ public class HibernateTest {
 			System.out.println(userDetail1.getUserName());
 		}
 
-		// Criteria criteria=session.createCriteria(UserDetail.class)
-		// .setProjection(Projections.property("userId"));
+		/*
+		 * Criteria criteria=session.createCriteria(UserDetail.class)
+		 * .setProjection(Projections.property("userId"));
+		 * 
+		 * Criteria criteria=session.createCriteria(UserDetail.class)
+		 * .setProjection(Projections.count("userId"));//property,max,min,count
+		 * etc
+		 */ Criteria criteria = session.createCriteria(UserDetail.class).addOrder(Order.desc("userId"));
 
-		// Criteria criteria=session.createCriteria(UserDetail.class)
-		// .setProjection(Projections.count("userId"));//property,max,min,count
-		// etc
-		Criteria criteria = session.createCriteria(UserDetail.class).addOrder(
-				Order.desc("userId"));
-
-		// criteria.add(Restrictions.eq("userName", "userName10"))
-		// .add(Restrictions.gt("userId", 5));
-		// criteria.add(Restrictions.like("userName", "%userName1%"))
-		// .add(Restrictions.between("userId", 1, 20));
-		// criteria.add(Restrictions.or(Restrictions.between("userId", 5, 7),
-		// Restrictions.between("userId", 14, 19)));
+		/*
+		 * criteria.add(Restrictions.eq("userName",
+		 * "userName10")).add(Restrictions.gt("userId", 5));
+		 * criteria.add(Restrictions.like("userName",
+		 * "%userName1%")).add(Restrictions.between("userId", 1, 20));
+		 * criteria.add(Restrictions.or(Restrictions.between("userId", 5, 7),
+		 * Restrictions.between("userId", 14, 19)));
+		 */
 
 		@SuppressWarnings("unchecked")
 		List<UserDetail> users = criteria.list();
