9280e7c0a9b1181a1a08fdc8d4e99dc7fad3980c
Sushil Kumar Bhaskar <write2sirfsushil@gmail.com>
2016-02-20 13:15:34 +0530

    WeakHashMap update

--- Collection/src/skbh/collection/WeakHashMapExample.java
+++ Collection/src/skbh/collection/WeakHashMapExample.java
@@ -3,17 +3,26 @@ package skbh.collection;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+/**
+ * Constructs a new WeakHashMap with the same mappings as the specified
+ * map. The WeakHashMap is created with the default load factor (0.75)
+ * and an initial capacity sufficient to hold the mappings in the specified map.
+ *
+ * @param weakHashMap  the map whose mappings are to be placed in this map
+ * @throws NullPointerException if the specified map is null
+ * @since 1.3
+ */
 public class WeakHashMapExample {
 
 	public static void main(String[] args) {
-		Map weakHashMap = new WeakHashMap();
+		Map<Object, String> weakHashMap = new WeakHashMap<Object, String>();
 		Integer number = new Integer(200);
-		weakHashMap.put("sushilKey", "sushil");
-		weakHashMap.put(new String("tempObject"), "temp value");
-		weakHashMap.put(number, "two hundred");
-		weakHashMap.put(new Float(256.00), "temp float value");
+		weakHashMap.put("sushilKey", "sushil");// Strong reference
+		weakHashMap.put(new String("tempObject"), "temp value");// due to weak reference it goes to garbage collector, System.gc()
+		weakHashMap.put(number, "two hundred");// Strong reference
+		weakHashMap.put(new Float(256.00), "temp float value");// due to weak reference it goes to garbage collector, System.gc()
 		System.out.println("Before Garbage Collector# " + weakHashMap);
-		System.gc();
+		System.gc();// called garbage collector
 		System.out.println("After Garbage Collector# " + weakHashMap);
 
 	}
