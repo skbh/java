52312c0813c076014eabff40f26f58a28c43a573
Sushil Kumar Bhaskar <write2sirfsushil@gmail.com>
2016-03-01 00:26:42 +0530

    ThreadPool Example

---
+++ Thread/src/in/skbh/exe/ThreadPoolExecutorFramwork.java
@@ -0,0 +1,46 @@
+package in.skbh.exe;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Process implements Runnable {
+	private int id = 0;
+
+	@Override
+	public void run() {
+		System.out.println("Starting # " + id);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("completed # " + id);
+
+	}
+
+	public Process(int id) {
+		this.id = id;
+	}
+
+}
+
+public class ThreadPoolExecutorFramwork {
+
+	public static void main(String[] args) {
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		for (int i = 0; i < 5; i++) {
+			executor.submit(new Process(i));
+		}
+		executor.shutdown();
+		System.out.println("All tak submitted");
+		try {
+			executor.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("All tak Completed");
+
+	}
+
+}
